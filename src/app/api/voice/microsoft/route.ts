dc31c13da0da98ba3a0eacf370ffd1797fd3d4d7
rdmclin2 <rdmclin2@gmail.com>
2024-04-11 23:12:43 +0800

    fix: use lobe tts

--- src/app/api/voice/microsoft/route.ts
+++ src/app/api/voice/microsoft/route.ts
@@ -1,46 +1,24 @@
-import { NextResponse } from 'next/server';
+import { MicrosoftSpeechTTS } from '@lobehub/tts';
+import { Buffer } from 'node:buffer';
 
-const { v4: uuidv4 } = require('uuid');
-const axios = require('axios');
+// Instantiate EdgeSpeechTTS
+const tts = new MicrosoftSpeechTTS({ locale: 'en-US' });
 
 export const POST = async (req: Request) => {
-  const { ssml } = await req.json();
-  const data = JSON.stringify({
-    offsetInPlainText: 0,
-    properties: {
-      SpeakTriggerSource: 'AccTuningPagePlayButton',
+  const { message, pitch, speed, voice } = await req.json();
+  // Create speech synthesis request payload
+  const payload = {
+    input: message,
+    options: {
+      voice: voice || 'en-US-GuyNeural',
+      pitch: (pitch - 1) / 2,
+      rate: speed - 1,
     },
-    ssml,
-    ttsAudioFormat: 'audio-24khz-160kbitrate-mono-mp3',
-  });
-
-  const config = {
-    data: data,
-    headers: {
-      accept: '*/*',
-      'accept-language': 'zh-CN,zh;q=0.9',
-      authority: 'southeastasia.api.speech.microsoft.com',
-      'content-type': 'application/json',
-      customvoiceconnectionid: uuidv4(),
-      origin: 'https://speech.microsoft.com',
-      'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-site',
-      'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-    },
-    method: 'post',
-    responseType: 'arraybuffer',
-
-    url: 'https://southeastasia.api.speech.microsoft.com/accfreetrial/texttospeech/acc/v3.0-beta1/vcg/speak',
   };
 
-  try {
-    return await axios(config);
-  } catch {
-    return NextResponse.json({ errorMessage: '转换失败', success: false }, { status: 400 });
-  }
+  // Call create method to synthesize speech
+  const response = await tts.create(payload);
+  const mp3Buffer = Buffer.from(await response.arrayBuffer());
+
+  return new Response(mp3Buffer);
 };
