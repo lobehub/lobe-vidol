3645f9a8340100abe18a1a8470f07cbefac9ed24
rdmclin2 <rdmclin2@gmail.com>
2024-04-18 23:09:03 +0800

    :sparkles: feat: 在我的中添加系统设置。

--- src/app/my/SideBar/MyList/index.tsx
+++ src/app/my/SideBar/MyList/index.tsx
@@ -1,4 +1,4 @@
-import { Bot, Mic2 } from 'lucide-react';
+import { Bot, Mic2, Settings2 } from 'lucide-react';
 import Link from 'next/link';
 import { memo } from 'react';
 
@@ -14,6 +14,7 @@ const MyList = memo<MyListProps>(({ activeTab, mobile }) => {
   const items = [
     { icon: Bot, label: '我的角色', value: MyTabs.Agent },
     { icon: Mic2, label: '我的舞蹈', value: MyTabs.Dance },
+    { icon: Settings2, label: '系统设置', value: MyTabs.Config },
   ];
 
   return items.map(({ value, icon, label }) => (
---
+++ src/app/my/config/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+import Config from '@/panels/ConfigPanel/Config';
+
+const Market = () => {
+  return <Config />;
+};
+
+export default Market;
