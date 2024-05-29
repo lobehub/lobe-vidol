aa228ba2dfe747731223f155b7103c9f14b7ae0e
rdmclin2 <rdmclin2@gmail.com>
2024-05-29 23:43:09 +0800

    :art: chore: remove touch props

--- src/constants/touch.tsx
+++ src/constants/touch.tsx
@@ -43,117 +43,95 @@ export const DEFAULT_TOUCH_ACTION_CONFIG: TouchActionConfig = {
   [TouchAreaEnum.Head]: [
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '哇!最喜欢摸摸头!',
     },
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '感觉又充满了力量呢!',
     },
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '哇塞，这个摸摸头的感觉好神奇!',
     },
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '摸摸头让我开心一整天!',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '听说被摸头是会长不高的呢!',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '干嘛戳我呀？',
     },
   ],
   [TouchAreaEnum.Arm]: [
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '啊，好喜欢呢~',
     },
     {
       emotion: VRMExpressionPresetName.Relaxed,
-      enabled: true,
       text: '主人的手好温暖啊~',
     },
     {
       emotion: VRMExpressionPresetName.Happy,
-      enabled: true,
       text: '哈哈，牵手让我感到快乐~',
     },
   ],
   [TouchAreaEnum.Leg]: [
     {
       emotion: VRMExpressionPresetName.Surprised,
-      enabled: true,
       text: '让我们保持纯洁的友谊不好吗？',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '喂，你是要作死吗?',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '主人的手又不听指挥了吗?',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '讨厌~会痒的啦~!',
     },
   ],
   [TouchAreaEnum.Chest]: [
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '不可以这样欺负我啦！快把手拿开！',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '幺幺零吗？这里有个变态一直在摸我！',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '再摸的话我可要报警了',
     },
     {
       emotion: VRMExpressionPresetName.Surprised,
-      enabled: true,
       text: '干嘛戳我呀！还能不能愉快地聊天了!',
     },
   ],
   [TouchAreaEnum.Belly]: [
     {
       emotion: VRMExpressionPresetName.Surprised,
-      enabled: true,
       text: '是不小心碰到的吧...',
     },
     {
       emotion: VRMExpressionPresetName.Angry,
-      enabled: true,
       text: '干嘛动我呀，小心我咬你哦！',
     },
     {
       emotion: VRMExpressionPresetName.Relaxed,
-      enabled: true,
       text: '醒醒，我们之间没有结果的!',
     },
     {
       emotion: VRMExpressionPresetName.Relaxed,
-      enabled: true,
       text: '讨厌！我可要生气啦！',
     },
   ],
-  enabled: true,
 };
