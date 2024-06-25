96ad19fde8c8b416fde967ff3c403080635473cb
Elegy <270663716@qq.com>
2024-06-25 23:37:40 +0800

    :sparkles: feat: add componets file i18n

--- src/components/ModelTag/index.tsx
+++ src/components/ModelTag/index.tsx
@@ -1,13 +1,15 @@
 import { Tag } from '@lobehub/ui';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import ModelIcon from './ModelIcon';
 
 interface ModelTagProps {
   model?: string;
 }
-const ModelTag = memo<ModelTagProps>(({ model }) => (
-  <Tag icon={<ModelIcon model={model} />}>{model ? model : '请选择模型'}</Tag>
-));
+const ModelTag = memo<ModelTagProps>(({ model }) => {
+  const { t } = useTranslation('common');
+  return <Tag icon={<ModelIcon model={model} />}>{model ? model : t('selectModel')}</Tag>;
+});
 
 export default ModelTag;
