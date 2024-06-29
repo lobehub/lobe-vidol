59ec5bf7cab9dcc93e1905d6e08ee6f58c366a3c
Elegy <270663716@qq.com>
2024-06-29 19:25:12 +0800

    :sparkles:feat: creat new i18n about features

---
+++ src/locales/default/features.ts
@@ -0,0 +1,25 @@
+export default {
+  share: {
+    screenshot: '截图',
+    shareGPT: '分享GPT',
+    withSystemRole: '包含助手角色设定',
+    withBackground: '包含背景图片',
+    withFooter: '包含页脚',
+    imageType: '图片格式',
+    downloadScreenshot: '下载截图',
+    shareToGPT: '生成 ShareGPT 分享链接',
+    share: '分享',
+    shareToMarket: '分享到助手市场',
+  },
+  submit: {
+    submitAssistant: '提交助手',
+    submitWarning: '请补全助手信息后提交，需要包含名称、描述、头像和封面',
+    assistantId: '助手标识符',
+    assistantIdTip: '请输入助手的标识符，需要是唯一的，比如 vidol-agent-klee',
+  },
+  theme: {
+    auto: '跟随系统',
+    light: '亮色模式',
+    dark: '暗黑模式',
+  },
+};
