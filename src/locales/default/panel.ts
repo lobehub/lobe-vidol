44bf58446723ff9a91dc8436234dca99384eb2de
Elegy <270663716@qq.com>
2024-06-29 12:23:22 +0800

    :sparkles:feat: add marketPanel i18n file & translate

--- src/locales/default/panel.ts
+++ src/locales/default/panel.ts
@@ -8,4 +8,14 @@ export default {
     findDance: '找到你最喜欢的舞蹈',
     musicAndDance: '音乐与舞蹈',
   },
+  market: {
+    findVidol: '找到你最爱的偶像',
+  },
+  role: {
+    uploadSize: '支持单个文件上传，推荐尺寸为 {{width}} * {{height}} 的倍数',
+    greetTip: '请输入角色与你打招呼时的用语',
+    roleReadmeTip: '请输入角色说明',
+    roleDescriptionTip: '请输入角色描述',
+    roleNameTip: '请输入角色名称',
+  },
 };
