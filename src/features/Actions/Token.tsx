c2db62cc3ff11a557f1899885d7d6c86b522bfd2
Elegy <270663716@qq.com>
2024-06-30 17:06:25 +0800

    :sparkles:feat: add token file i18n tanslate

--- src/features/Actions/Token.tsx
+++ src/features/Actions/Token.tsx
@@ -1,5 +1,6 @@
 import { TokenTag } from '@lobehub/ui';
 import { isEqual } from 'lodash-es';
+import { useTranslation } from 'react-i18next';
 
 import { OPENAI_MODEL_LIST } from '@/constants/openai';
 import { useCalculateToken } from '@/hooks/useCalculateToken';
@@ -8,12 +9,12 @@ import { configSelectors, useSettingStore } from '@/store/setting';
 const Token = () => {
   const config = useSettingStore((s) => configSelectors.currentOpenAIConfig(s), isEqual);
   const usedTokens = useCalculateToken();
-
+  const { t } = useTranslation('features');
   return (
     <TokenTag
       maxValue={OPENAI_MODEL_LIST.find((item) => item.id === config?.model)?.tokens || 4096}
       value={usedTokens}
-      text={{ overload: 'Token 超出', remained: 'Token 剩余', used: 'Token 已使用' }}
+      text={{ overload: t('token.overload'), remained: t('token.remained'), used: t('token.used') }}
     />
   );
 };
--- src/features/Actions/TokenMini.tsx
+++ src/features/Actions/TokenMini.tsx
@@ -1,5 +1,6 @@
 import { Tooltip, Typography } from 'antd';
 import { isEqual } from 'lodash-es';
+import { useTranslation } from 'react-i18next';
 
 import { OPENAI_MODEL_LIST } from '@/constants/openai';
 import { useCalculateToken } from '@/hooks/useCalculateToken';
@@ -9,12 +10,14 @@ const TokenMini = () => {
   const config = useSettingStore((s) => configSelectors.currentOpenAIConfig(s), isEqual);
   const usedTokens = useCalculateToken();
   const maxValue = OPENAI_MODEL_LIST.find((item) => item.id === config?.model)?.tokens || 4096;
+  const { t } = useTranslation('features');
 
   return (
-    <Tooltip title={`消耗 Token 数量计算，包括消息，角色设定与上下文：${usedTokens} / ${maxValue}`}>
-      <Typography.Text type={'secondary'}>Token Count: {usedTokens}</Typography.Text>
+    <Tooltip title={t('token.useToken', { usedTokens, maxValue })}>
+      <Typography.Text type={'secondary'}>
+        {t('token.tokenCount')}: {usedTokens}
+      </Typography.Text>
     </Tooltip>
   );
 };
-
 export default TokenMini;
