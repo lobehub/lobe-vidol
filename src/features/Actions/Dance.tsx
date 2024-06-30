b19d7aeffd41a8561661f4f194bbe83a06bb19d9
Elegy <270663716@qq.com>
2024-06-30 16:15:31 +0800

    :sparkles:feat: add dance,discord,history file i18n tanslate

--- src/features/Actions/Dance.tsx
+++ src/features/Actions/Dance.tsx
@@ -1,18 +1,19 @@
 import { ActionIcon } from '@lobehub/ui';
 import { Music } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 import { useGlobalStore } from '@/store/global';
 
 export default () => {
   const [openPanel] = useGlobalStore((s) => [s.openPanel]);
-
+  const { t } = useTranslation('panel');
   return (
     <ActionIcon
       icon={Music}
       onClick={() => {
         openPanel('dance');
       }}
-      title={'音乐与舞蹈'}
+      title={t('dance.musicAndDance')}
     />
   );
 };
