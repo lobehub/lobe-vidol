e097d917a3ba4db545dd8f988f92ae9066950ef9
rdmclin2 <rdmclin2@gmail.com>
2024-07-15 22:49:49 +0800

    :sparkles: feat: 添加角色设定模板

--- src/components/agent/AgentMeta/style.ts
+++ src/components/agent/AgentMeta/style.ts
@@ -10,7 +10,7 @@ export const useStyles = createStyles(({ css, token }) => ({
     line-height: 1;
   `,
   desc: css`
-    max-width: 480px;
+    width: 480px;
     margin-top: ${token.marginXXS}px;
 
     font-size: ${token.fontSizeSM}px;
--- src/panels/RolePanel/RoleEdit/Role/SystemRole/index.tsx
+++ src/panels/RolePanel/RoleEdit/Role/SystemRole/index.tsx
@@ -1,6 +1,8 @@
+import { Card, Cards } from '@lobehub/ui/mdx';
 import { Input } from 'antd';
-import React, { CSSProperties, memo } from 'react';
+import React, { CSSProperties, memo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Flexbox } from 'react-layout-kit';
 
 import { MAX_SYSTEM_ROLE_LENGTH } from '@/constants/common';
 import { agentSelectors, useAgentStore } from '@/store/agent';
@@ -12,23 +14,56 @@ interface Props {
 
 export default memo<Props>((props) => {
   const { style, className } = props;
+  const inputRef = useRef(null);
   const [agent, updateAgentConfig] = useAgentStore((s) => [
     agentSelectors.currentAgentItem(s),
     s.updateAgentConfig,
   ]);
-  const { t } = useTranslation('panel');
+  const { t } = useTranslation(['panel', 'role']);
   return (
-    <Input.TextArea
-      className={className}
-      style={style}
-      value={agent?.systemRole}
-      autoSize={{ minRows: 16 }}
-      placeholder={t('role.inputRoleSetting')}
-      showCount
-      maxLength={MAX_SYSTEM_ROLE_LENGTH}
-      onChange={(e) => {
-        updateAgentConfig({ systemRole: e.target.value });
-      }}
-    />
+    <Flexbox>
+      <Input.TextArea
+        ref={inputRef}
+        className={className}
+        style={style}
+        value={agent?.systemRole}
+        autoSize={{ minRows: 16 }}
+        placeholder={t('role.inputRoleSetting')}
+        showCount
+        maxLength={MAX_SYSTEM_ROLE_LENGTH}
+        onChange={(e) => {
+          updateAgentConfig({ systemRole: e.target.value });
+        }}
+      />
+      <Cards style={{ marginTop: 24 }}>
+        <Card
+          image="https://r2.vidol.chat/common/default.png"
+          title={t('systemRole.defaultLabel', { ns: 'role' })}
+          onClick={() => {
+            updateAgentConfig({
+              systemRole: t('systemRole.default', { ns: 'role', char: agent?.meta.name }),
+            });
+          }}
+        />
+        <Card
+          image="https://r2.vidol.chat/common/genshin.png"
+          title={t('systemRole.geniusLabel', { ns: 'role' })}
+          onClick={() => {
+            updateAgentConfig({
+              systemRole: t('systemRole.genius', { ns: 'role', char: agent?.meta.name }),
+            });
+          }}
+        />
+        <Card
+          image="https://r2.vidol.chat/common/zzz.png"
+          title={t('systemRole.zzzLabel', { ns: 'role' })}
+          onClick={() => {
+            updateAgentConfig({
+              systemRole: t('systemRole.zzz', { ns: 'role', char: agent?.meta.name }),
+            });
+          }}
+        />
+      </Cards>
+    </Flexbox>
   );
 });
