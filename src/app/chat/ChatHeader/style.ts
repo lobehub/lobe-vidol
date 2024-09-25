ab1eed4d8772cc235547e9bed33b0126d68d81e3
rdmclin2 <rdmclin2@gmail.com>
2024-09-25 08:36:48 +0800

    feat: background style

--- src/app/chat/ChatHeader/style.ts
+++ src/app/chat/ChatHeader/style.ts
@@ -1,5 +1,4 @@
 import { createStyles } from 'antd-style';
-import { rgba } from 'polished';
 
 import { CHAT_HEADER_HEIGHT } from '@/constants/token';
 
@@ -7,7 +6,6 @@ const useStyles = createStyles(({ token, css }) => ({
   header: css`
     height: ${CHAT_HEADER_HEIGHT}px;
     padding: ${token.paddingSM}px;
-    background-color: ${rgba(token.colorBgLayout, 0.4)};
   `,
   player: css`
     min-width: 480px;
