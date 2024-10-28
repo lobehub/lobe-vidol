8862ec85aa3a0c55a6c81f4b2c5b2a7e81940b60
rdmclin2 <rdmclin2@gmail.com>
2024-10-28 08:28:19 +0800

    chore: fix sidebar panel

--- src/app/chat/ViewerMode/style.ts
+++ src/app/chat/ViewerMode/style.ts
@@ -21,10 +21,11 @@ export const useStyles = createStyles(({ css, token }) => ({
     }
   `,
   dialog: css`
-    z-index: 1;
+    z-index: 2;
     margin: ${CHAT_HEADER_HEIGHT}px auto 0;
   `,
   docker: css`
+    z-index: 2;
     width: 100%;
     padding: ${token.paddingSM}px;
   `,
