ca5e2c06a62f0f1fcef0f3e4ea6373cec491e763
rdmclin2 <rdmclin2@gmail.com>
2024-11-07 22:29:12 +0800

    chore: support i18n locales

--- src/services/agent.ts
+++ src/services/agent.ts
@@ -1,16 +1,26 @@
 import { AGENT_MARKET_URL } from '@/constants/common';
+import { isLocaleNotSupport } from '@/constants/locale';
+import { Locales, normalizeLocale } from '@/locales/resources';
 
 /**
  * 请求线上 Agent index
  */
-export const getAgentIndex = async (url: string = `${AGENT_MARKET_URL}/index.json`) => {
+export const getAgentIndex = async (locale: Locales) => {
+  const url = isLocaleNotSupport(locale)
+    ? AGENT_MARKET_URL
+    : `${AGENT_MARKET_URL}/index.${normalizeLocale(locale)}.json`;
+
   const res = await fetch(url);
 
   return res.json();
 };
 
-export const getAgentDetail = async (id: string) => {
-  const res = await fetch(`${AGENT_MARKET_URL}/${id}.json`);
+export const getAgentDetail = async (id: string, locale: Locales) => {
+  const url = isLocaleNotSupport(locale)
+    ? `${AGENT_MARKET_URL}/${id}.json`
+    : `${AGENT_MARKET_URL}/${id}.${normalizeLocale(locale)}.json`;
+
+  const res = await fetch(url);
 
   return res.json();
 };
