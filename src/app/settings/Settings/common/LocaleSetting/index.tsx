e0c65226ab5903fc004e7b157fcd4e79557a2356
rdmclin2 <rdmclin2@gmail.com>
2024-11-09 08:43:14 +0800

    fix: 通过 router.refresh 更新路由 i18n

--- src/app/settings/Settings/common/LocaleSetting/index.tsx
+++ src/app/settings/Settings/common/LocaleSetting/index.tsx
@@ -1,10 +1,12 @@
 import { Select } from 'antd';
 import { isEqual } from 'lodash-es';
+import { useRouter } from 'next/navigation';
 import React, { CSSProperties, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { localeOptions } from '@/locales/resources';
 import { useSettingStore } from '@/store/setting';
+import { LocaleMode } from '@/types/locale';
 
 interface Props {
   style?: CSSProperties;
@@ -14,14 +16,20 @@ export default memo<Props>((props) => {
   const { style } = props;
   const { t } = useTranslation('settings');
   const options = [{ label: t('common.theme.locale.auto'), value: 'auto' }, ...localeOptions];
+  const router = useRouter();
 
   const [locale, switchLocale] = useSettingStore((s) => [s.config.locale, s.switchLocale], isEqual);
+
+  const handleChange = (value: string) => {
+    switchLocale(value as LocaleMode);
+    router.refresh();
+  };
 
   return (
     <Select
       style={style}
       defaultValue={'auto'}
-      onChange={switchLocale}
+      onChange={handleChange}
       options={options}
       value={locale}
     />
