586f8433bdc288e1365c4b5e525629a24892b7aa
rdmclin2 <rdmclin2@gmail.com>
2024-11-21 22:15:41 +0800

    fix: provider auth

--- src/app/(backend)/api/chat/[provider]/route.ts
+++ src/app/(backend)/api/chat/[provider]/route.ts
@@ -1,6 +1,5 @@
 import { createErrorResponse } from '@/app/(backend)/api/errorResponse';
 import { checkAuth } from '@/app/(backend)/middleware/auth';
-import { JWTPayload } from '@/constants/auth';
 import { AgentRuntime, ChatCompletionErrorPayload } from '@/libs/agent-runtime';
 import { ChatErrorType } from '@/types/fetch';
 import { ChatStreamPayload } from '@/types/provider/chat';
@@ -9,41 +8,33 @@ import { initAgentRuntimeWithUserPayload } from '../agentRuntime';
 
 export const runtime = 'edge';
 
-export const POST = checkAuth(
-  async (
-    req: Request,
-    {
-      params,
-      createRuntime,
-    }: { createRuntime?: (payload: JWTPayload) => AgentRuntime; params: { provider: string } },
-  ) => {
-    const { provider } = params;
+export const POST = checkAuth(async (req: Request, { params, jwtPayload, createRuntime }) => {
+  const { provider } = params;
 
-    const payload = (await req.json()) as JWTPayload;
-    console.log('payload', payload);
-    try {
-      // ============  1. init chat model   ============ //
-      let agentRuntime: AgentRuntime;
-      if (createRuntime) {
-        agentRuntime = createRuntime(payload);
-      } else {
-        agentRuntime = await initAgentRuntimeWithUserPayload(provider, payload);
-      }
-
-      // ============  2. create chat completion   ============ //
-      return await agentRuntime.chat(payload as ChatStreamPayload);
-    } catch (e) {
-      const {
-        errorType = ChatErrorType.InternalServerError,
-        error: errorContent,
-        ...res
-      } = e as ChatCompletionErrorPayload;
-
-      const error = errorContent || e;
-      // track the error at server side
-      console.error(`Route: [${provider}] ${errorType}:`, error);
-
-      return createErrorResponse(errorType, { error, ...res, provider });
+  try {
+    // ============  1. init chat model   ============ //
+    let agentRuntime: AgentRuntime;
+    if (createRuntime) {
+      agentRuntime = createRuntime(jwtPayload);
+    } else {
+      agentRuntime = await initAgentRuntimeWithUserPayload(provider, jwtPayload);
     }
-  },
-);
+
+    // ============  2. create chat completion   ============ //
+    const data = (await req.json()) as ChatStreamPayload;
+
+    return await agentRuntime.chat(data);
+  } catch (e) {
+    const {
+      errorType = ChatErrorType.InternalServerError,
+      error: errorContent,
+      ...res
+    } = e as ChatCompletionErrorPayload;
+
+    const error = errorContent || e;
+    // track the error at server side
+    console.error(`Route: [${provider}] ${errorType}:`, error);
+
+    return createErrorResponse(errorType, { error, ...res, provider });
+  }
+});
