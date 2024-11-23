2a3f4944cfb585f15cdaa5ddba3df0d449a7706d
rdmclin2 <rdmclin2@gmail.com>
2024-11-23 10:51:16 +0800

    chore: update test case

--- src/server/utils/url.ts
+++ src/server/utils/url.ts
@@ -1,5 +1,9 @@
 import urlJoin from 'url-join';
 
-const siteUrl = 'https://vidol.lobehub.com';
+const isVercelPreview = process.env.VERCEL === '1' && process.env.VERCEL_ENV !== 'production';
+
+const vercelPreviewUrl = `https://${process.env.VERCEL_URL}`;
+
+export const siteUrl = isVercelPreview ? vercelPreviewUrl : 'https://vidol.lobehub.com';
 
 export const getCanonicalUrl = (...paths: string[]) => urlJoin(siteUrl, ...paths);
