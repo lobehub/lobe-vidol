4df9bb319713f4c8cbbbf69d722f847632408cec
rdmclin2 <rdmclin2@gmail.com>
2024-11-29 23:23:09 +0800

    feat: add live2d sdk

--- src/libs/trpc/client.ts
+++ src/libs/trpc/client.ts
@@ -6,6 +6,7 @@ import type { EdgeRouter } from '@/server/routers';
 export const edgeClient = createTRPCClient<EdgeRouter>({
   links: [
     httpBatchLink({
+      // @ts-ignore
       transformer: superjson,
       url: '/trpc/edge',
     }),
