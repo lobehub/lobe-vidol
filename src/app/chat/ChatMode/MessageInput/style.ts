a9dae790a2ec24c30c229ab57e71f5690dde50bf
rdmclin2 <rdmclin2@gmail.com>
2024-12-03 23:42:14 +0800

    feat: optimize mobile style

--- src/app/chat/ChatMode/MessageInput/style.ts
+++ src/app/chat/ChatMode/MessageInput/style.ts
@@ -2,6 +2,7 @@ import { createStyles } from 'antd-style';
 
 export const useStyles = createStyles(({ css, token }) => ({
   alert: css`
+    width: 100%;
     color: ${token.colorTextTertiary};
   `,
 }));
