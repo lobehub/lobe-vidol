839b5f93bf29c674455f8b3673ea83ba7f62b349
rdmclin2 <rdmclin2@gmail.com>
2024-12-08 23:07:50 +0800

    fix: vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,27 @@
 import { ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface';
+import { cookies } from 'next/headers';
 import { PropsWithChildren } from 'react';
+import { isRtlLang } from 'rtl-detect';
 
 import Analytics from '@/components/Analytics';
 import NProgress from '@/components/NProgress';
+import { DEFAULT_LANG, LOBE_LOCALE_COOKIE } from '@/constants/locale';
 import PWAInstall from '@/features/PWAInstall';
 import Layout from '@/layout';
 import { isMobileDevice } from '@/utils/server/responsive';
 
 import StyleRegistry from './StyleRegistry';
 
-const RootLayout = ({ children }: PropsWithChildren) => {
+const RootLayout = async ({ children }: PropsWithChildren) => {
   // get default theme config to use with ssr
+  const cookieStore = await cookies();
 
+  const lang = cookieStore.get(LOBE_LOCALE_COOKIE);
+  const locale = lang?.value || DEFAULT_LANG;
+
+  const direction = isRtlLang(locale) ? 'rtl' : 'ltr';
   return (
-    <html lang="cn" suppressHydrationWarning>
+    <html lang={locale} dir={direction} suppressHydrationWarning>
       <head>
         <script src="https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js" defer />
       </head>
