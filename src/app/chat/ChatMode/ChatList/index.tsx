abb61ccf266a479026f3b01ce4884f3b49c0cf5b
rdmclin2 <rdmclin2@gmail.com>
2024-12-13 22:49:51 +0800

    fix: chatlist loading

--- src/app/chat/ChatMode/ChatList/index.tsx
+++ src/app/chat/ChatMode/ChatList/index.tsx
@@ -5,18 +5,19 @@ import { Flexbox } from 'react-layout-kit';
 
 import SkeletonList from './SkeletonList';
 
+const Loading = () => (
+  <div style={{ margin: '0 auto' }}>
+    <SkeletonList />
+  </div>
+);
+
+const ChatList = dynamic(() => import('./VirtualizedList'), {
+  ssr: false,
+  loading: Loading,
+});
+
 const Conversation = memo(() => {
   const { mobile } = useResponsive();
-  const Loading = () => (
-    <div style={{ margin: '0 auto' }}>
-      <SkeletonList mobile={mobile} />
-    </div>
-  );
-
-  const ChatList = dynamic(() => import('./VirtualizedList'), {
-    ssr: false,
-    loading: Loading,
-  });
 
   return (
     <Flexbox
